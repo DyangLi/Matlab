d4b75d32695b7de1a8fcffc0e0e52cb20f007a58
Dongyang Li <arfartom@gmail.com>
2014-09-16 16:07:44 -0700

    FCCS fitting 3D with separate inputs

--- FCCS_fitting_3d.m
+++ FCCS_fitting_3d.m
@@ -1,21 +1,23 @@
-function f_x = FCCS_fitting_3d(data, name)
+function f_x = FCCS_fitting_3d(data_x, data_a, data_b, name)
 opt = statset('nlinfit');
-opt.MaxIter = 5000;
+%opt.MaxIter = 5000;
 %input: raw data, names for final output
 %output: f_x 
 %% importing data, define range of data for fitting
-trace = data(:,2:2:end);
-corr_time = data(:,1);
+trace_x = data_x(:,2:2:end);
+trace_a = data_a(:,2:2:end);
+trace_b = data_b(:,2:2:end);
+corr_time = data_x(:,1);
 F3d = @(a,t) a(1)*(1./(1+t/a(2))).*sqrt(1./(1+t/25*a(2)));
 tau_fit_end = find(corr_time > 500, 1, 'first')-1;
 
 %% NL fitting, estimate tau as point ~= 1/e G(0)
-tau_est = find(mean(trace(1:tau_fit_end, 2:3:end),2) < 0.4*mean(mean(trace(1:5, 2:3:end))), 1, 'first');
-g_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace(1:tau_fit_end, 2:3:end),2), F3d, [mean(mean(trace(1:5, 2:3:end))), 1],opt);
-tau_est = find(mean(trace(1:tau_fit_end, 3:3:end),2) < 0.4*mean(mean(trace(1:5, 3:3:end))), 1, 'first');
-r_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace(1:tau_fit_end, 3:3:end),2), F3d, [mean(mean(trace(1:5, 3:3:end))), 1],opt);
-tau_est = find(mean(trace(1:tau_fit_end, 1:3:end),2) < 0.4*mean(mean(trace(1:5, 21:3:end))), 1, 'first');
-x_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace(1:tau_fit_end, 1:3:end),2), F3d, [mean(mean(trace(1:5, 1:3:end))), 1],opt);
+tau_est = find(mean(trace_x(1:tau_fit_end, 2:2:end),2) < 0.4*mean(mean(trace_x(1:5, 2:3:end))), 1, 'first');
+g_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace_x(1:tau_fit_end, 2:3:end),2), F3d, [mean(mean(trace_x(1:5, 2:3:end))), 10],opt);
+tau_est = find(mean(trace_a(1:tau_fit_end, 3:3:end),2) < 0.4*mean(mean(trace_a(1:5, 3:3:end))), 1, 'first');
+r_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace_a(1:tau_fit_end, 3:3:end),2), F3d, [mean(mean(trace_a(1:5, 3:3:end))), 10],opt);
+tau_est = find(mean(trace_b(1:tau_fit_end, 1:3:end),2) < 0.4*mean(mean(trace_b(1:5, 21:3:end))), 1, 'first');
+x_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace_b(1:tau_fit_end, 1:3:end),2), F3d, [mean(mean(trace_b(1:5, 1:3:end))), 10],opt);
 %% Plotting
 figure; 
 h = plot(corr_time, mean(trace(:,2:3:end),2), 'g', corr_time, mean(trace(:,3:3:end),2), 'r', corr_time, mean(trace(:,1:3:end),2), 'b');
