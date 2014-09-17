bef266d433e41aadc26fbaffac26925ffa6a43e5
Dongyang Li <arfartom@gmail.com>
2014-09-16 21:21:11 -0700

    bug fix - FCCS 3d fitting

--- FCCS_fitting_3d.m
+++ FCCS_fitting_3d.m
@@ -12,15 +12,15 @@ F3d = @(a,t) a(1)*(1./(1+t/a(2))).*sqrt(1./(1+t/25*a(2)));
 tau_fit_end = find(corr_time > 500, 1, 'first')-1;
 
 %% NL fitting, estimate tau as point ~= 1/e G(0)
-tau_est = find(mean(trace_x(1:tau_fit_end, 2:2:end),2) < 0.4*mean(mean(trace_x(1:5, 2:3:end))), 1, 'first');
-g_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace_x(1:tau_fit_end, 2:3:end),2), F3d, [mean(mean(trace_x(1:5, 2:3:end))), 10],opt);
-tau_est = find(mean(trace_a(1:tau_fit_end, 3:3:end),2) < 0.4*mean(mean(trace_a(1:5, 3:3:end))), 1, 'first');
-r_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace_a(1:tau_fit_end, 3:3:end),2), F3d, [mean(mean(trace_a(1:5, 3:3:end))), 10],opt);
-tau_est = find(mean(trace_b(1:tau_fit_end, 1:3:end),2) < 0.4*mean(mean(trace_b(1:5, 21:3:end))), 1, 'first');
-x_beta = nlinfit(corr_time(1:tau_fit_end), mean(trace_b(1:tau_fit_end, 1:3:end),2), F3d, [mean(mean(trace_b(1:5, 1:3:end))), 10],opt);
+tau_est = find(nanmean(trace_x(1:tau_fit_end, :),2) < 0.4*nanmean(nanmean(trace_x(1:5,:))), 1, 'first');
+x_beta = nlinfit(corr_time(1:tau_fit_end), nanmean(trace_x(1:tau_fit_end, :),2), F3d, [nanmean(nanmean(trace_x(1:5, :))), 10],opt);
+tau_est = find(nanmean(trace_a(1:tau_fit_end, :),2) < 0.4*nanmean(nanmean(trace_a(1:5, :))), 1, 'first');
+g_beta = nlinfit(corr_time(1:tau_fit_end), nanmean(trace_a(1:tau_fit_end, :),2), F3d, [nanmean(nanmean(trace_a(1:5, :))), 10],opt);
+tau_est = find(nanmean(trace_b(1:tau_fit_end, :),2) < 0.4*nanmean(nanmean(trace_b(1:5, :))), 1, 'first');
+r_beta = nlinfit(corr_time(1:tau_fit_end), nanmean(trace_b(1:tau_fit_end, :),2), F3d, [nanmean(nanmean(trace_b(1:5, :))), 10],opt);
 %% Plotting
 figure; 
-h = plot(corr_time, mean(trace(:,2:3:end),2), 'g', corr_time, mean(trace(:,3:3:end),2), 'r', corr_time, mean(trace(:,1:3:end),2), 'b');
+h = plot(corr_time, mean(trace_a,2), 'g', corr_time, mean(trace_b,2), 'r', corr_time, mean(trace_x,2), 'b');
 set(gca, 'XScale', 'log');
 f_x = 0.5*(x_beta(1)/g_beta(1) + x_beta(1)/r_beta(1));
 y_ax = ylim;
